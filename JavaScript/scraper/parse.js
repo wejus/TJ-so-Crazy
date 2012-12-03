17d5c4fef952ad21419ac94dec8136c1b1b7d844
Wejus <wejus@umich.edu>
2012-12-02 23:51:11 -0600

    Fixed spaces and commented out console.log()s except for final recipe result.

--- JavaScript/scraper/parse.js
+++ JavaScript/scraper/parse.js
@@ -8,19 +8,19 @@ var source = sourceBuffer.toString();
 
 //Find Recipe Title
 var recipeTitle = $(source).find('title').html().match(/(.*)-/)[1].trim();
-console.log(recipeTitle);
+//console.log(recipeTitle);
 
 //Get Ingredients
 var materialIngredients = [];
 $(source).find('ul.ingredient-wrap').find('span.ingredient-name').each(function(index,value) {
-  materialIngredients[index]=$(value).html();
+  materialIngredients[index] = $(value).html();
 });
 //console.log(materialIngredients);
 
 //Get Quantities
 var amounts = [];
 $(source).find('ul.ingredient-wrap').find('span.ingredient-amount').each(function(index,value) {
-  amounts[index]=$(value).html();
+  amounts[index] = $(value).html();
 });
 //console.log(amounts);
 
@@ -29,26 +29,26 @@ var ingredients = [];
 $(materialIngredients).each(function(index,value) {
   ingredients[index] = amounts[index] + ' ' + value;
 });
-console.log(ingredients);
+//console.log(ingredients);
 
 //Get Directions
 var directions = [];
 $(source).find('div.directions').find('span.plaincharacterwrap').each(function(index,value) {
   directions[index] = $(value).html();
 });
-console.log(directions);
+//console.log(directions);
 
 //Get Prep Time
-var prepTime = $(source).find('#prepMinsSpan').html().replace(/<[^>]*>/g, "");
-console.log(prepTime); 
+var prepTime = $(source).find('#prepMinsSpan').html().replace(/<[^>]*>/g, '');
+//console.log(prepTime); 
 
 //Get Cook Time
-var cookTime = $(source).find('#cookMinsSpan').html().replace(/<[^>]*>/g, "");
-console.log(cookTime);
+var cookTime = $(source).find('#cookMinsSpan').html().replace(/<[^>]*>/g, '');
+//console.log(cookTime);
 
 //Get Total Time Until Ready
-var readyTime = $(source).find('#totalHoursSpan').html().replace(/<[^>]*>/g, "");
-console.log(readyTime); 
+var readyTime = $(source).find('#totalHoursSpan').html().replace(/<[^>]*>/g, '');
+//console.log(readyTime); 
 
 //Create Recipe Object
 var recipe = {
